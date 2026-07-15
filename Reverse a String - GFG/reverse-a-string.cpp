78eeffbcdcf9f1e459d3bf0efe327889603f7ba7
Joohhnnyyy <johnsonansh32@gmail.com>
2026-07-15 14:26:38 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -2,11 +2,7 @@ class Solution {
   public:
     string reverseString(string& s) {
         // code here
-        string str = s;
-        int i = 0;
-        int j = s.size() - 1;
-        while( i< j){
-            swap(str[i++] , str[j--]);
-        }return str;
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
