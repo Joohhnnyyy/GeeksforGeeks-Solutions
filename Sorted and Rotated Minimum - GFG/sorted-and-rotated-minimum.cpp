69af5c3267549b121966206f7f4cda0a80ec1648
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-10 16:29:03 +0530

    Added solution - LeetHub

---
+++ Sorted and Rotated Minimum - GFG/sorted-and-rotated-minimum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        // code here
+        int left = 0 , right = arr.size() - 1;
+        while(left < right ){
+            int mid = left + ( right - left ) / 2;
+            if( arr[mid] <= arr[right])right = mid;
+            else left = mid + 1;
+        }return arr[left];
+    }
+};
